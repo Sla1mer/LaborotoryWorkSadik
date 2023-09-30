fcd72f9dddc8911533071b1cb4875dc4a4a6468f
Sla1mer <slameryt4@gmail.com>
2023-09-30 10:09:13 +0200

    Лабораторная работа №5

--- Lab5Sadik/Program.cs
+++ Lab5Sadik/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Высокий уровень 2
+
+Console.Write("Введите двузначное число: ");
+int number = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+
+if (number / 10 == 5 || number % 10 == 5)
+{
+    Console.WriteLine($"Число 5 присутсвует в числе {number}");
+}
+
+if (number / 10 == a || number % 10 == a)
+{
+    Console.WriteLine($"Число {a} присутсвует в числе {number}");
+}
