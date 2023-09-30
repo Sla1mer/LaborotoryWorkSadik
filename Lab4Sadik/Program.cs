8b1e188f94a0b0a82fbcf0a5e9c1ee2357d602a9
Назар Садик <Slameryt4@gmail.com>
2023-09-30 14:56:39 +0200

    Лабораторная работа №4

---
+++ Lab4Sadik/Program.cs
@@ -0,0 +1,9 @@
+﻿// Высокий уровень 8
+
+Console.Write("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+
+Console.Write("Введите y: ");
+double y = double.Parse(Console.ReadLine());
+
+Console.WriteLine((x >= -1 && x <= 1 && y >= 0 && y <= 2) ? "Входит" : "Не входит");
