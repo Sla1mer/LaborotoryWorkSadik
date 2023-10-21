4cb6fca3c26fac5d8c099fcb2c570ab059d9b9ad
Назар Садик <Slameryt4@gmail.com>
2023-10-22 00:24:02 +0200

    9

---
+++ Lab9Sadik/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+    Console.Write("Введите x: ");
+    double x = double.Parse(Console.ReadLine());
+
+    Console.Write("Введите n: ");
+    int n = int.Parse(Console.ReadLine());
+
+    double sum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum += Math.Cos(i * 2 * x) / 3 + 8 * (n - 1);
+    }
+
+    Console.WriteLine($"Сумма: {sum:F2}");
+} catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
