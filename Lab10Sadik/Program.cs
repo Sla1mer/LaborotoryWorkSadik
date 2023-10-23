4bf6d06c80e0e865228dca8b3d692fad2fd14e0b
Назар Садик <Slameryt4@gmail.com>
2023-10-23 19:13:25 +0200

    10

---
+++ Lab10Sadik/Program.cs
@@ -0,0 +1,28 @@
+﻿// Высокий уровень
+// Вариант 18
+
+Console.WriteLine("|    f(x)     |");
+
+double x = 0.5;
+
+while(x <= 2)
+{
+    double fx = 0;
+
+    for(int k = 1; k <= 5; k++)
+    {
+        int factorial = 1;
+
+        for (int i = 1; i <= k; i++)
+        {
+            factorial *= i;
+        }
+        
+
+        fx += (Math.Pow(-1.5, k) * Math.Pow(x, k)) / factorial;
+    }
+
+    Console.WriteLine($"|    {fx:F2}    |");
+
+    x += 0.15;
+}
