2c78ea56719792c4f9a0c66f1a8168ae365c1c54
Назар Садик <Slameryt4@gmail.com>
2023-11-08 17:55:06 +0200

    12 лабораторная

---
+++ Lab12Sadik/Program.cs
@@ -0,0 +1,21 @@
+﻿// Высокий уровень 
+// Вариант 15
+
+int n = int.Parse(Console.ReadLine());
+
+int[,] mas = new int[n, n];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (j + i == n - 1) mas[i, j] = 1;
+        else if (j + i > n - 1)
+        {
+            mas[i, j] = 2;
+
+        }
+        else mas[i, j] = 0;
+        Console.Write(mas[i, j] + " ");
+    }
+    Console.WriteLine();
+}
