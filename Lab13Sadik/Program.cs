6b57dfa6077131d4098707189af65df6cd5a0abb
Your Name <slameryt4@gmail.com>
2023-11-11 16:00:57 +0200

    fix

--- Lab13Sadik/Program.cs
+++ Lab13Sadik/Program.cs
@@ -2,16 +2,13 @@
 // 2 Вариант
 
 char[] mas = new char[7];
-
-for (int i = 0; i < 7; i++)
-{
-    Console.WriteLine($"Введите {i + 1} элемент массива");
-    mas[i] = char.Parse(Console.ReadLine());
-}
+Console.Write("Введите строку: ");
+mas = Console.ReadLine().ToLower().ToCharArray();
 
 bool isSymmetrical = true;
 for (int i = 0; i < mas.Length / 2; i++)
 {
+
     if (mas[i] != mas[mas.Length - i - 1])
     {
         isSymmetrical = false;
