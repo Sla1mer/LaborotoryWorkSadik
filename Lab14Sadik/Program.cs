6e0ea5126c693b579b2071134596d5d735744801
Your Name <slameryt4@gmail.com>
2023-11-20 18:42:48 +0200

    Lab14Sadik

---
+++ Lab14Sadik/Program.cs
@@ -0,0 +1,22 @@
+﻿// Высокий уровень
+// Вариант 2
+
+Console.Write("Введите первую строку:");
+string str1 = Console.ReadLine();
+
+Console.Write("Введите вторую строку:");
+string str2 = Console.ReadLine();
+
+string res = "";
+
+for (int i = 0; i < str1.Length; i++)
+{
+    char currentChar = str1[i];
+
+    if (str2.Contains(currentChar.ToString()) && !res.Contains(currentChar.ToString()))
+    {
+        res += currentChar;
+    }
+}
+
+Console.WriteLine($"Результат: {res}");
